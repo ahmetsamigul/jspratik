4ec9ec4c2a6f88a5e128a26f6b08ff24605b6af1
Ahmet Sami Gül <a_sami_gul@Ahmet-MacBook-Air-4.local>
2025-01-31 20:14:57 +0300

    eleman silme

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-
-const cardBody = document.querySelectorAll(".card-body")[1];
 const todoList = document.querySelector(".list-group");
+const todos = document.querySelectorAll(".list-group-item");
 
-const link = document.createElement("a");
-link.id = "googleLink";
-link.className = "btn btn-dark btn-sml mt-3";
-link.href = "https://www.w3schools.com/js/js_objects.asp";
-link.target = "_blank";
-link.innerHTML = "Site";
 
-cardBody.appendChild(link);
+// todos[todos.length-1].remove();
 
-const todo = document.createElement("li");
-const todoLink = document.createElement("a");
-const i = document.createElement("i");
+console.log(todos);
 
-todo.className = "list-group-item d-flex justify-content-between";
-todo.innerHTML = "Todo 5";
 
-todoLink.href = "#";
-todoLink.className = "delete-item";
-
-i.className = "fa fa-remove";
-
-todoLink.appendChild(i);
-todo.appendChild(todoLink);
-
-todoList.appendChild(todo);
-console.log(link);
 
